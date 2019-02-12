f9cf7105dc65759dc2d9b7a52b4c8c54552896e5
cajablo <caroline.jablonicky@gmail.com>
2019-02-11 18:25:05 -0800

    added state map, need to improve legend and breaks

--- oil_production_exploration.R
+++ oil_production_exploration.R
@@ -6,6 +6,7 @@ source("setup.R")
 #Load us oil production csv 
 raw_data <- read.csv("us_oil_production_state.csv", check.names=FALSE, header = TRUE, stringsAsFactors = FALSE) %>%
   #use gather to tidy the data, so one row per year and state
+  ##length(.) gets the number of rows in the dataframe
   gather("year", "oil_produced", 2:length(.))
 
 #Plot how much oil produced yearly in all of US
@@ -64,7 +65,9 @@ Top_5_states_annual_production <- raw_data %>%
 
 Top_5_states_annual_production
 
-#Make a map of of oil production by state
+###################
+#Make a map of of oil production by state in 2016 -------------------------
+###################
 
 #Load "fiftystater" which allows you to create a map w/Hawaii and Alaska as an inset
 devtools::install_github("wmurphyrd/fiftystater")
@@ -84,3 +87,78 @@ sf_fifty <- st_as_sf(fifty_states, coords = c("long", "lat"), crs = 4326) %>%
   # convert polygons to multipolygons for states with discontinuous regions
   group_by(id, State) %>%
   summarize() 
+
+#filter oil data to 2016 and join to spatial data
+oil_prod_states_sf_2016 <- raw_data %>%
+  filter(year==2016)%>%
+  right_join(sf_fifty, by="State") %>%
+  select(-id)
+
+#divide data into quantiles
+#to get 4 states per section use 8 quantiles
+quantiles <- quantile(oil_prod_states_sf_2016$oil_produced, probs=seq(0,1,length.out = 8), na.rm=TRUE)
+
+#create breaks that are slight prettier, easier to read than the raw quantile values
+pretty_breaks <- c(250, 2000, 10000, 25000, 50000, 150000)
+minVal <- min(oil_prod_states_sf_2016$oil_produced, na.rm=T)
+maxVal <- max(oil_prod_states_sf_2016$oil_produced, na.rm=T)
+
+brks<- c(minVal, pretty_breaks, maxVal)
+#create a blank label vector and then fill in based using the breaks produced above, rounding the label to the nearest 10th 
+labels <- c()
+
+for(idx in 1:length(brks)){
+  labels <- c(labels, round(brks[idx+1], 2))
+}
+#remove NA
+labels <- labels[1:length(labels)-1]
+
+#add column to data frame that labels each row in the appropriate quantile/label group
+oil_prod_states_sf_2016$breaks_2016 <- cut(oil_prod_states_sf_2016$oil_produced,
+                                              breaks=brks,
+                                              labels=labels,
+                                              include.lowest=TRUE)
+
+#set scale for plotting
+brks_scale <- levels(oil_prod_states_sf_2016$breaks_2016)
+labels_scale <- rev(format(as.numeric(brks_scale), big.mark = ","))
+
+#plot 2016 oil production
+ggplot() +
+  geom_sf(data=oil_prod_states_sf_2016, aes(fill=breaks_2016), colour = "darkgray") +
+  coord_sf(datum = NA) +
+  theme_void() +
+  theme(legend.position = "bottom") +
+  labs(
+    x = NULL, 
+    y = NULL,
+    title = "Crude Oil Production",
+    subtitle = "2016",
+    caption = "Data: US Energy Information Administration"
+  ) +
+  scale_fill_manual(
+    values=rev(viridis(10, alpha = 0.8)[2:9]),
+    breaks = rev(brks_scale),
+    name= "Oil Produced (thousand barrels)", 
+    drop=FALSE,
+    labels=labels_scale, 
+    guide = guide_legend(
+      direction = "horizontal",
+      keyheight = unit(2, units = "mm"),
+      keywidth = unit(20/length(labels), units = "mm"),
+      title.position = 'top',
+      title.hjust = 0.5,
+      label.hjust = 1,
+      nrow = 1,
+      byrow = T, 
+      reverse = T,
+      label.position = "bottom"
+    )
+  ) +
+  theme (
+    text = element_text(color = "#22211d"), 
+    plot.title = element_text(size= 18, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
+    plot.subtitle = element_text(size= 14, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
+    plot.caption = element_text( size=10, color = "#4e4d47")
+  )
+
